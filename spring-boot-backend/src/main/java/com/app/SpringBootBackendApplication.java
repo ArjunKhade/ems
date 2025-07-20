fe7e19c532c571d63316d32496b65bd2bc7e25e2
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-07-20 15:43:59 +0530

    added confige for war deployement

--- spring-boot-backend/src/main/java/com/app/SpringBootBackendApplication.java
+++ spring-boot-backend/src/main/java/com/app/SpringBootBackendApplication.java
@@ -4,15 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class SpringBootBackendApplication {
+public class SpringBootBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
 	}
+
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootBackendApplication.class);
+    }
 	
 	
 	// add a method to configure ModelMapper bean as a spring bean (to be managed by SC)
