e5bee6c1e46c376fa2465b8239ab92f78fe606c7
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-11-22 13:03:43 +0530

    modifield header component

--- frontend/src/layout/header/header.component.ts
+++ frontend/src/layout/header/header.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app/app.service';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatMenuModule, MatIconModule, NgbPopoverModule],
+  imports: [MatButtonModule,CommonModule, MatMenuModule, MatIconModule, NgbPopoverModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  isHandset!: Observable<boolean>;
 
   user: string | null = ''; isPopoverOpen = false;
   constructor(private appService: AppService, private router: Router){
    this.user = localStorage.getItem("user");
+  }
+  ngOnInit(): void {
+    this.isHandset = this.appService.isHandset$;
   }
  onSearch(event: any) {
   debugger
